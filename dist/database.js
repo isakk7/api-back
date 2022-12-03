d15873e9512a16740b8a51bcceeca3fc22508af7
isaacal <isaac.lopez.its@gmail.com>
2022-12-03 16:32:18 -0600

    repo finished

---
+++ dist/database.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startConnection = void 0;
+const mongoose_1 = require("mongoose");
+async function startConnection() {
+    await (0, mongoose_1.connect)('mongodb://localhost/photo-gallery-db', {
+    // useNewUrlParser: true,
+    //   useFindAndModify: false
+    });
+    console.log('Database is connected');
+}
+exports.startConnection = startConnection;
